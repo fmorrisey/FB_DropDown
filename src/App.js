969f1f617229bfc1d8c56600746cfcce7f8aed95
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-05-06 13:45:47 -0500

    Dropdown menu inital complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { ReactComponent as BellIcon } from "./icons/bell.svg";
 import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
 import { ReactComponent as CaretIcon } from "./icons/caret.svg";
@@ -7,6 +6,9 @@ import { ReactComponent as CogIcon } from "./icons/cog.svg";
 import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
 import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
 import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
   return (
@@ -44,9 +46,21 @@ function Navitem(props) {
 }
 
 function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
   function DropdownItem(props) {
     return (
-      <a href="#" className="menu-item">
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
         <span className="icon-button">{props.leftIcon}</span>
 
         {props.children}
@@ -56,11 +70,47 @@ function DropdownMenu() {
     );
   }
   return (
-    <div className="dropdown">
-      <DropdownItem>My Profile</DropdownItem>
-      <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />}>
-        Settings
-      </DropdownItem>
+    <div className="dropdown" style={{ height: menuHeight }}>
+      {/* Main Menu */}
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+
+        // This allows React to toggle the classes
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      {/* Settings Menu */}
+      <CSSTransition
+        in={activeMenu === "settings"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+        // This allows React to toggle the classes
+      >
+        <div className="menu">
+          <DropdownItem leftIcon={<ArrowIcon />} goToMenu="main" />
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
