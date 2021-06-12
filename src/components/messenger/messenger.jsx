69f6d09d007d4fed276f04c04c742406c33f0314
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-06-11 19:00:19 -0500

    Active icons

--- src/components/messenger/messenger.jsx
+++ src/components/messenger/messenger.jsx
@@ -6,11 +6,24 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 export default function messenger(props) {
   const friendsList = friends.map((item, index) => {
+    function ActiveUser(props) {
+      return (
+        <div className="active-container">
+          {item.active ? (
+            <span className="active-dot"></span>
+          ) : (
+            <span className="active-last">{item.last_active}m </span>
+          )}
+        </div>
+      );
+    }
+
     return (
       <div key={index} className="friend">
         <div className="header">
           <span className="icon-account icon-left">
             <AccountCircleIcon />
+            <ActiveUser />
           </span>
           <span className="username">{item.user}</span>
         </div>
