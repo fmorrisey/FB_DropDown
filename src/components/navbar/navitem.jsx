4401a732905403fe45fdbfb8a869ca8df360413e
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-06-15 12:19:02 -0500

    User profile img added to nav

--- src/components/navbar/navitem.jsx
+++ src/components/navbar/navitem.jsx
@@ -1,22 +1,37 @@
+import { KeyboardReturnRounded } from "@material-ui/icons";
 import React, { useState } from "react";
+import UsrImg from "../../assets/users/forrest.jpeg";
 
 export default function Navitem(props) {
   const [open, setOpen] = useState(false);
 
-  return (
-    <li className="nav-item">
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a
-        href="#"
-        className={
-          open ? " icon-button__BG icon-button-active" : " icon-button__BG"
-        }
-        onClick={() => setOpen(!open)}
-      >
-        {props.icon}
-      </a>
+  if (props.user) {
+    const User = { ...props.user[0].user };
 
-      {open && props.children}
-    </li>
-  );
+    return (
+      <li className="nav-user">
+        <img src={UsrImg} className="user-avatar-navbar" />
+        {`${User.firstName}`}
+      </li>
+    );
+  }
+
+  if (props.icon) {
+    return (
+      <li className="nav-item">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a
+          href="#"
+          className={
+            open ? " icon-button__BG icon-button-active" : " icon-button__BG"
+          }
+          onClick={() => setOpen(!open)}
+        >
+          {props.icon}
+        </a>
+
+        {open && props.children}
+      </li>
+    );
+  }
 }
