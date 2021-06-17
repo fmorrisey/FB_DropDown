4ce9c074e06848484838e88a47f6448b057fc708
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-06-17 12:56:54 -0500

    Added a comments section to each post item, includes a refactor of some styling

--- src/components/newsfeed/post.jsx
+++ src/components/newsfeed/post.jsx
@@ -11,14 +11,14 @@ import CallMadeIcon from "@material-ui/icons/CallMade";
 
 export default function Posts(props) {
   // Feed data is passed from App.js -> newsfeed -> posts
-  const posts = props.feed.map((item, index) => {
+  const posts = props.feed.map((postItem, index) => {
     function PostImage(props) {
       console.log(props.image);
       if (props.image) {
         return (
           <div key={`post-${index}`} className="post-image">
             <a className="img-wrapper" href="">
-              <img src={item.src} alt={item.type} />
+              <img src={postItem.src} alt={postItem.type} />
             </a>
           </div>
         );
@@ -28,6 +28,40 @@ export default function Posts(props) {
       }
     }
 
+    function Comments(props) {
+      return props.postComments.map((comment, index) => {
+        console.log("comment", comment.isLiked);
+        return (
+          <>
+            <div key={index} className="post-comment">
+              <AccountCircleIcon className="icon-account__small icon-left" />
+              <div>
+                <div className="post-comment__content">
+                  <a href="">
+                    <b>{comment.user}</b>
+                  </a>
+                  <br />
+                  {comment.content}
+                </div>
+                <div className="post-comment__engagement">
+                  {comment.isLiked ? (
+                    <a href="" className="fbBlue">
+                      Like
+                    </a>
+                  ) : (
+                    <a href="" className="">
+                      Like
+                    </a>
+                  )}{" "}
+                  · Reply · {comment.time}
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      });
+    }
+
     return (
       <div key={index} className="post">
         <div className="header">
@@ -35,9 +69,9 @@ export default function Posts(props) {
             <AccountCircleIcon />
           </span>
           <div className="top">
-            <span className="username">{item.user}</span>
+            <span className="username">{postItem.user}</span>
             <span className="time">
-              {item.time} · <Privacy access={item.privacy} />{" "}
+              {postItem.time} · <Privacy access={postItem.privacy} />{" "}
             </span>
           </div>
           <a className="icon-button__noBG icon-right">
@@ -45,23 +79,24 @@ export default function Posts(props) {
           </a>
         </div>
         <div className="content">
-          <p>{item.post}</p>
-          <PostImage image={item.src} />
+          <p>{postItem.post}</p>
+          <PostImage image={postItem.src} />
         </div>
 
         <hr />
         <div className="post-engagement">
-          <span className="post-item">
+          <span className="post-engagement-action">
             <ThumbUpAltOutlinedIcon className="icon-like" /> Like
           </span>
-          <span className="post-item">
+          <span className="post-engagement-action">
             <ChatBubbleOutlineOutlinedIcon className="icon-comment" /> Comment
           </span>
-          <span className="post-item">
+          <span className="post-engagement-action">
             <CallMadeIcon className="icon-share" /> Share
           </span>
         </div>
         <hr />
+        <Comments postComments={postItem.comments} />
         <div className="post-comment">
           <span className="icon-account__small icon-left">
             <AccountCircleIcon />
