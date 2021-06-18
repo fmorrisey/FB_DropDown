93cf8da3d975143d3784c93c0e2169eff4cf044e
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-06-18 11:58:43 -0500

    Added Facebook logo and search bar too the navigation

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./navbar.scss";
 
+import { CSSTransition } from "react-transition-group";
+
+import FBLogo from "../../assets/icons/fblogo58.png";
+import SearchIcon from "@material-ui/icons/Search";
+
 export default function Navbar(props) {
   return (
     <nav className="navbar">
+      <img src={FBLogo} className="logo" />
+
+      <span className="search">
+        <SearchIcon className="search-icon" />
+        <input
+          type="search"
+          placeholder="Search Forrestbook"
+          className="search-input"
+        />
+      </span>
+
       <ul className="navbar-nav"> {props.children} </ul>
     </nav>
   );
