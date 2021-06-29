9aff8d45f5bcbef35a92bc6b43952dcbd5f790dd
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-06-29 08:53:23 -0500

    Code comments for the privacy component

--- src/components/newsfeed/privacy.jsx
+++ src/components/newsfeed/privacy.jsx
@@ -1,3 +1,18 @@
+/*
+    PRIVACY COMPONENT
+
+    This component returns the corresponding
+    privacy icon based on the feed.json config
+    back to the post component in the header section
+
+    Currently has three know privacy access states
+    with a null || undefined fall back.
+
+    1) Public
+    2) PublicFriends
+    3) Private
+*/
+
 import React from "react";
 import feed from "../../assets/data/feed.json";
 import "./newsfeed.scss";
@@ -7,6 +22,7 @@ import PublicIcon from "@material-ui/icons/Public";
 import LockIcon from "@material-ui/icons/Lock";
 
 export default function Privacy(props) {
+  // Public access
   if (props.access === "public") {
     return (
       <span className="icon-privacy">
@@ -14,6 +30,7 @@ export default function Privacy(props) {
       </span>
     );
   }
+  // Friends access
   if (props.access === "friends") {
     return (
       <span className="icon-privacy">
@@ -21,6 +38,7 @@ export default function Privacy(props) {
       </span>
     );
   }
+  // Private access
   if (props.access === "private") {
     return (
       <span className="icon-privacy">
@@ -28,7 +46,10 @@ export default function Privacy(props) {
       </span>
     );
   }
-  if (null) {
+
+  // Fall back returns NULL
+  // to assist in catching errors
+  if (null || undefined) {
     return "NULL";
   }
 }
