ec3d249b408ddf7d2b6714b6cec4198d39dbb18f
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-09-03 16:28:14 -0500

    Added a prototype filtering function

--- src/components/newsfeed/newsfeed.jsx
+++ src/components/newsfeed/newsfeed.jsx
@@ -14,11 +14,18 @@ import Posts from "./post";
 
 export default function Newsfeed(props) {
   console.log("feed here", props.feed);
+
+  const filtered = props.feed.filter(
+    (post) => post.user !== "Forrest Morrisey"
+  );
+
+  console.table("FILTER here", filtered);
+
   return (
     <div className="feed">
       {/* #TODO: Add filtering */}
       <CreatePost user={props.user} />
-      <Posts feed={props.feed} />
+      <Posts feed={filtered} />
     </div>
   );
 }
